f74545690f2ed9b8cb67cc7fc14fc867fcb57c7d
Zhaofang He <151109521+iwsanren@users.noreply.github.com>
2024-10-11 10:28:08 +0100

    202 1
    Hash table review

--- HashTable/src/happyNumber.java
+++ HashTable/src/happyNumber.java
@@ -2,30 +2,57 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class happyNumber {
-    public boolean isHappy(int n) {
-        Set<Integer> res = new HashSet<>();
-        //当n！=1 且 res不包含n的时候继续求n各位平方和。一旦进入循环就退出。
-        while(n!=1 && !res.contains(n)){
-            res.add(n);
+    //    public boolean isHappy(int n) {
+//        Set<Integer> res = new HashSet<>();
+//        //当n！=1 且 res不包含n的时候继续求n各位平方和。一旦进入循环就退出。
+//        while(n!=1 && !res.contains(n)){
+//            res.add(n);
+//            n = getSum(n);
+//        }
+//        //如果n==1，return true
+//        return n==1;
+//    }
+//    //求出n各位的平方和
+//    public int getSum(int n){
+//        int sum = 0;
+//        while (n > 0){
+//            int temp = n % 10;
+//            sum += temp * temp;
+//            n = n/10;
+//        }
+//        return sum;
+//    }
+    //24.10.11 review
+    public static boolean isHappy(int n) {
+        Set<Integer> hash = new HashSet<>();
+        n = getSum(n);
+        //当平方和不为1且hash中不包含n的时候继续循环
+        while (n != 1 && !hash.contains(n)) {
+            hash.add(n);
+            //System.out.println("After getSum: " + n);
             n = getSum(n);
         }
-        //如果n==1，return true
-        return n==1;
+        //System.out.println(n);
+        return n == 1 ? true : false;
+
     }
-    //求出n各位的平方和
-    public int getSum(int n){
+
+    public static int getSum(int n) {
         int sum = 0;
-        while (n > 0){
+        while(n != 0){
             int temp = n % 10;
             sum += temp * temp;
-            n = n/10;
+            n /= 10;
         }
         return sum;
     }
+
+
 }
-class happyNumberTest{
+
+class happyNumberTest {
     public static void main(String[] args) {
-        //System.out.println(happyNumber.isHappy(19));
+        System.out.println(happyNumber.isHappy(25));
     }
 
 }
--- HashTable/src/twoSum.java
+++ HashTable/src/twoSum.java
@@ -2,28 +2,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class twoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
-        if(nums == null || nums.length == 0) return res;
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] res = new int[2];
+//        if(nums == null || nums.length == 0) return res;
+//
+//        Map<Integer,Integer> record = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            int temp = target-nums[i];
+//            if(record.containsKey(temp)){
+//                res[1] = i;
+//                res[0] = record.get(temp);
+//                break;
+//            }
+//            record.put(nums[i],i);
+//        }
+//        return res;
+//    }
 
-        Map<Integer,Integer> record = new HashMap<>();
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        if(nums == null || nums.length == 0) return result;
+
+        Map<Integer,Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int temp = target-nums[i];
-            if(record.containsKey(temp)){
-                res[1] = i;
-                res[0] = record.get(temp);
-                break;
+            int temp = target - nums[i];
+            if(hash.containsKey(temp)){
+                result[0] = hash.get(temp);
+                result[1] = i;
             }
-            record.put(nums[i],i);
+            hash.put(nums[i],i);//要放在判断后，不然会重复使用某个=target/2的元素。
         }
-        return res;
+        return result;
     }
+
 }
 class twoSumTest{
     public static void main(String[] args) {
-//        int[] res = twoSum.twoSum(new int[]{2,3,5,7,8},9);
-//        System.out.println(res[0]);
-//        System.out.println(res[1]);
+        int[] res = twoSum.twoSum(new int[]{3,2,4},6);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
 
     }
 }
